b2f0ab57b62b4089d5373c9aedaebdce9ae8546a
Manoj Jayapprakash <manoj.jayapprakash@gmail.com>
2021-07-26 13:30:33 +0530

    Added CSS stylings and layouts

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import './App.css';
 
 function App() {
   return (
-    <main id="quote-box">
-      <h1 id="text">
-        <i class="fas fa-quote-left"></i>I didn’t fail the test. I just found
-        100 ways to do it wrong.
-      </h1>
-      <p id="author">- Benjamin Franklin</p>
-      <button id="new-quote">New Quote</button>
-      <a
-        href="twitter.com/intent/tweet"
-        id="tweet-quote"
-        title="Tweet this quote"
-        target="_blank"
-      >
-        <i className="fab fa-twitter"></i>
-      </a>
+    <main className="app">
+      <section id="quote-box">
+        <h1 id="text">
+          <i className="fas fa-quote-left"></i> I didn’t fail the test. I just
+          found 100 ways to do it wrong.
+        </h1>
+        <p id="author">- Benjamin Franklin</p>
+        <div className="actions">
+          <a
+            href="twitter.com/intent/tweet"
+            id="tweet-quote"
+            title="Tweet this quote"
+            target="_blank"
+            className="box-items"
+          >
+            <i className="fab fa-twitter "></i>
+          </a>
+          <button id="new-quote" className="box-items">
+            New Quote
+          </button>
+        </div>
+      </section>
     </main>
   );
 }
