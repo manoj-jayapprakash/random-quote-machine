92eb93805be9c075693c78482e50512b8ca04597
Manoj Jayapprakash <manoj.jayapprakash@gmail.com>
2021-07-26 12:50:55 +0530

    HTML elements and structure created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main id="quote-box">
+      <h1 id="text">
+        <i class="fas fa-quote-left"></i>I didn’t fail the test. I just found
+        100 ways to do it wrong.
+      </h1>
+      <p id="author">- Benjamin Franklin</p>
+      <button id="new-quote">New Quote</button>
+      <a
+        href="twitter.com/intent/tweet"
+        id="tweet-quote"
+        title="Tweet this quote"
+        target="_blank"
+      >
+        <i className="fab fa-twitter"></i>
+      </a>
+    </main>
+  );
 }
 
 export default App;
